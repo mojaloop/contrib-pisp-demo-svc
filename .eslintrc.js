28e28d59fee0fe41cb8566301268b63fd66bb217
Steven Wijaya <stevenwjy149@gmail.com>
2020-06-24 12:12:33 +0800

    chore: add commitlint and update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   plugins: [
+    '@typescript-eslint',
     'cucumber',
-    '@typescript-eslint'
+    'prettier',
+    'promise',
   ],
   extends: [
-    'eslint:recommended',
     'standard',
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     // Enforces ES6+ import/export syntax
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
+    // Promise
+    'plugin:promise/recommended',
+    // Prettier
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
@@ -32,6 +37,7 @@ module.exports = {
     'cucumber/expression-type': 2,
     'cucumber/no-restricted-tags': [2, 'wip', 'broken', 'foo'],
     'cucumber/no-arrow-functions': 2,
+    'prettier/prettier': 'error',
   },
   overrides: [
     {
