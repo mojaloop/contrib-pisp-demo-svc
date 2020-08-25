fe77c44861c735af59624b7865706b72d97f31eb
akapur99 <abhi.kapur09@gmail.com>
2020-08-25 16:23:02 +0530

    feat: add updateConsentById method to interface

--- src/repositories/consent.ts
+++ src/repositories/consent.ts
@@ -31,6 +31,14 @@ import { logger } from '~/shared/logger'
 
 export interface IConsentRepository {
   /**
+   * Updates a consent document based on a unique identifier.
+   *
+   * @param id    Id for the consent document that needs to be updated.
+   * @param data  Document fields that are about to be updated.
+   */
+  updateConsentById(id: string, data: Record<string, any>): Promise<void>
+
+  /**
    * Retrieves a consent document based on its consent ID.
    *
    * @param id    Consent ID of the document that needs to be retrieved.
@@ -59,7 +67,10 @@ export class FirebaseConsentRepository implements IConsentRepository {
     })
   }
 
-  async updateConsentById(id: string, data: Consent): Promise<void> {
+  async updateConsentById(
+    id: string,
+    data: Record<string, any>
+  ): Promise<void> {
     await firebase.firestore().collection('consents').doc(id).update(data)
   }
 }
