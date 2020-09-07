cc55900bec0b01b2b0db3078ed93d28cd7f8e604
akapur99 <abhi.kapur09@gmail.com>
2020-09-07 16:40:00 +0530

    test: refactor to improve test readability and add structure

--- test/unit/server/handlers/firestore/consents.test.ts
+++ test/unit/server/handlers/firestore/consents.test.ts
@@ -74,253 +74,269 @@ describe('Handlers for consent documents in Firebase', () => {
     jest.clearAllTimers()
   })
 
-  it('Should set status and consentRequestId for new consent', () => {
-    const consentRepositorySpy = jest.spyOn(
-      consentRepository,
-      'updateConsentById'
-    )
+  describe('OnCreate', () => {
+    it('Should set status and consentRequestId for new consent', () => {
+      const consentRepositorySpy = jest.spyOn(
+        consentRepository,
+        'updateConsentById'
+      )
 
-    consentsHandler.onCreate(server, {
-      id: '111',
-      userId: 'bob123',
-      party: {
-        partyIdInfo: {
-          partyIdType: PartyIdType.OPAQUE,
-          partyIdentifier: 'bob1234',
-          fspId: 'fspb',
+      consentsHandler.onCreate(server, {
+        id: '111',
+        userId: 'bob123',
+        party: {
+          partyIdInfo: {
+            partyIdType: PartyIdType.OPAQUE,
+            partyIdentifier: 'bob1234',
+            fspId: 'fspb',
+          },
         },
-      },
+      })
+
+      expect(consentRepositorySpy).toBeCalledWith('111', {
+        consentRequestId: '12345',
+        status: ConsentStatus.PENDING_PARTY_LOOKUP,
+      })
+    })
+  })
+
+  describe('OnUpdate', () => {
+    describe('Party Lookup', () => {
+      it('Should perform party lookup when all necessary fields are set', async () => {
+        const mojaloopClientSpy = jest
+          .spyOn(server.app.mojaloopClient, 'getParties')
+          .mockImplementation()
+
+        const validatorSpy = jest
+          .spyOn(Validator, 'isValidPartyLookup')
+          .mockReturnValue(true)
+
+        const consentPartyLookup: Consent = {
+          id: documentId,
+          userId: 'bob123',
+          party: {
+            partyIdInfo: {
+              partyIdType: PartyIdType.OPAQUE,
+              partyIdentifier: 'bob1234',
+            },
+          },
+          consentRequestId: '12345',
+          status: ConsentStatus.PENDING_PARTY_LOOKUP,
+        }
+
+        await consentsHandler.onUpdate(server, consentPartyLookup)
+
+        expect(validatorSpy).toBeCalledWith(consentPartyLookup)
+        expect(mojaloopClientSpy).toBeCalledWith(PartyIdType.OPAQUE, 'bob1234')
+      })
     })
 
-    expect(consentRepositorySpy).toBeCalledWith('111', {
-      consentRequestId: '12345',
-      status: ConsentStatus.PENDING_PARTY_LOOKUP,
+    describe('Authentication', () => {})
+
+    describe('Consent Request', () => {
+      it('Should initiate consent request request when all necessary fields are set', async () => {
+        const mojaloopClientSpy = jest
+          .spyOn(server.app.mojaloopClient, 'postConsentRequests')
+          .mockImplementation()
+
+        const validatorSpy = jest
+          .spyOn(Validator, 'isValidConsentRequest')
+          .mockReturnValue(true)
+
+        // Mock consent data that would be given by Firebase
+        const consentConsentRequest: Consent = {
+          id: documentId,
+          userId: 'bob123',
+          party: {
+            partyIdInfo: {
+              partyIdType: PartyIdType.OPAQUE,
+              partyIdentifier: 'bob1234',
+              fspId: 'fspb',
+            },
+          },
+          scopes: [
+            {
+              accountId: 'as2342',
+              actions: ['account.getAccess', 'account.transferMoney'],
+            },
+            {
+              accountId: 'as22',
+              actions: ['account.getAccess'],
+            },
+          ],
+          consentRequestId: '12345',
+          authChannels: ['WEB'],
+          accounts: [
+            { id: 'bob.aaaaa.fspb', currency: Currency.SGD },
+            { id: 'bob.bbbbb.fspb', currency: Currency.USD },
+          ],
+          status: ConsentStatus.PENDING_PARTY_CONFIRMATION,
+        }
+
+        // Mock the expected transaction request being sent.
+        const consentRequest: SDKStandardComponents.PostConsentRequestsRequest = {
+          initiatorId: consentConsentRequest.initiatorId as string,
+          id: consentConsentRequest.id,
+          scopes: consentConsentRequest.scopes as TCredentialScope[],
+          authChannels: consentConsentRequest.authChannels as TAuthChannel[],
+          callbackUri: config.get('mojaloop').callbackUri,
+        }
+
+        await consentsHandler.onUpdate(server, consentConsentRequest)
+
+        expect(mojaloopClientSpy).toBeCalledWith(
+          consentRequest,
+          consentConsentRequest.party?.partyIdInfo.fspId
+        )
+        expect(validatorSpy).toBeCalledWith(consentConsentRequest)
+      })
     })
-  })
 
-  it('Should perform party lookup when all necessary fields are set', async () => {
-    const mojaloopClientSpy = jest
-      .spyOn(server.app.mojaloopClient, 'getParties')
-      .mockImplementation()
+    describe('Challenge Generation Request', () => {
+      it('Should initiate challenge generation request when all necessary fields are set', async () => {
+        const mojaloopClientSpy = jest
+          .spyOn(server.app.mojaloopClient, 'postGenerateChallengeForConsent')
+          .mockImplementation()
 
-    const validatorSpy = jest
-      .spyOn(Validator, 'isValidPartyLookup')
-      .mockReturnValue(true)
+        const validatorSpy = jest
+          .spyOn(Validator, 'isValidChallengeGeneration')
+          .mockReturnValue(true)
 
-    const consentPartyLookup: Consent = {
-      id: documentId,
-      userId: 'bob123',
-      party: {
-        partyIdInfo: {
-          partyIdType: PartyIdType.OPAQUE,
-          partyIdentifier: 'bob1234',
-        },
-      },
-      consentRequestId: '12345',
-      status: ConsentStatus.PENDING_PARTY_LOOKUP,
-    }
+        // Mock consent data that would be given by Firebase
+        const consentGenerateChallenge: Consent = {
+          id: '111',
+          consentId: '2323',
+          party: {
+            partyIdInfo: {
+              partyIdType: PartyIdType.MSISDN,
+              partyIdentifier: '+1-222-222-2222',
+              fspId: 'fspb',
+            },
+          },
+          status: ConsentStatus.ACTIVE,
+          scopes: [
+            {
+              accountId: 'as2342',
+              actions: ['account.getAccess', 'account.transferMoney'],
+            },
+            {
+              accountId: 'as22',
+              actions: ['account.getAccess'],
+            },
+          ],
+          consentRequestId: '12345',
+          authChannels: ['WEB'],
+          accounts: [
+            { id: 'bob.aaaaa.fspb', currency: Currency.SGD },
+            { id: 'bob.bbbbb.fspb', currency: Currency.USD },
+          ],
+        }
 
-    await consentsHandler.onUpdate(server, consentPartyLookup)
+        await consentsHandler.onUpdate(server, consentGenerateChallenge)
 
-    expect(validatorSpy).toBeCalledWith(consentPartyLookup)
-    expect(mojaloopClientSpy).toBeCalledWith(PartyIdType.OPAQUE, 'bob1234')
-  })
+        expect(validatorSpy).toBeCalledWith(consentGenerateChallenge)
+        expect(mojaloopClientSpy).toBeCalledWith(
+          consentGenerateChallenge.consentId,
+          consentGenerateChallenge.party?.partyIdInfo.fspId
+        )
+      })
+    })
 
-  it('Should initiate consent request request when all necessary fields are set', async () => {
-    const mojaloopClientSpy = jest
-      .spyOn(server.app.mojaloopClient, 'postConsentRequests')
-      .mockImplementation()
+    describe('Signed Challenge', () => {
+      it('Should initiate PUT consent/{ID} request when challenge has been signed and all necessary fields are set', async () => {
+        const mojaloopClientSpy = jest
+          .spyOn(server.app.mojaloopClient, 'putConsentId')
+          .mockImplementation()
 
-    const validatorSpy = jest
-      .spyOn(Validator, 'isValidConsentRequest')
-      .mockReturnValue(true)
+        const validatorSpy = jest
+          .spyOn(Validator, 'isValidSignedChallenge')
+          .mockReturnValue(true)
 
-    // Mock consent data that would be given by Firebase
-    const consentConsentRequest: Consent = {
-      id: documentId,
-      userId: 'bob123',
-      party: {
-        partyIdInfo: {
-          partyIdType: PartyIdType.OPAQUE,
-          partyIdentifier: 'bob1234',
-          fspId: 'fspb',
-        },
-      },
-      scopes: [
-        {
-          accountId: 'as2342',
-          actions: ['account.getAccess', 'account.transferMoney'],
-        },
-        {
-          accountId: 'as22',
-          actions: ['account.getAccess'],
-        },
-      ],
-      consentRequestId: '12345',
-      authChannels: ['WEB'],
-      accounts: [
-        { id: 'bob.aaaaa.fspb', currency: Currency.SGD },
-        { id: 'bob.bbbbb.fspb', currency: Currency.USD },
-      ],
-      status: ConsentStatus.PENDING_PARTY_CONFIRMATION,
-    }
+        // Mock the expected transaction request being sent.
+        const consentVerifiedChallenge = {
+          id: '111',
+          consentId: '2323',
+          initiatorId: 'pispa',
+          participantId: 'pispb',
+          scopes: [
+            {
+              accountId: 'as2342',
+              actions: ['account.getAccess', 'account.transferMoney'],
+            },
+            {
+              accountId: 'as22',
+              actions: ['account.getAccess'],
+            },
+          ],
+          party: {
+            partyIdInfo: {
+              partyIdType: PartyIdType.MSISDN,
+              partyIdentifier: '+1-222-222-2222',
+              fspId: 'fspb',
+            },
+          },
+          status: ConsentStatus.CHALLENGE_VERIFIED,
+          credential: {
+            id: '9876',
+            credentialType: 'FIDO' as const,
+            status: 'VERIFIED' as const,
+            challenge: {
+              payload: 'string_representing_challenge_payload',
+              signature: 'string_representing_challenge_signature',
+            },
+            payload: 'string_representing_credential_payload',
+          },
+        }
 
-    // Mock the expected transaction request being sent.
-    const consentRequest: SDKStandardComponents.PostConsentRequestsRequest = {
-      initiatorId: consentConsentRequest.initiatorId as string,
-      id: consentConsentRequest.id,
-      scopes: consentConsentRequest.scopes as TCredentialScope[],
-      authChannels: consentConsentRequest.authChannels as TAuthChannel[],
-      callbackUri: config.get('mojaloop').callbackUri,
-    }
+        await consentsHandler.onUpdate(server, consentVerifiedChallenge)
 
-    await consentsHandler.onUpdate(server, consentConsentRequest)
+        expect(validatorSpy).toBeCalledWith(consentVerifiedChallenge)
+        expect(mojaloopClientSpy).toBeCalledWith(
+          consentVerifiedChallenge.consentId,
+          {
+            requestId: consentVerifiedChallenge.id,
+            initiatorId: consentVerifiedChallenge.initiatorId,
+            participantId: consentVerifiedChallenge.participantId,
+            scopes: consentVerifiedChallenge.scopes,
+            credential: consentVerifiedChallenge.credential,
+          },
+          consentVerifiedChallenge.party.partyIdInfo.fspId
+        )
+      })
+    })
 
-    expect(mojaloopClientSpy).toBeCalledWith(
-      consentRequest,
-      consentConsentRequest.party?.partyIdInfo.fspId
-    )
-    expect(validatorSpy).toBeCalledWith(consentConsentRequest)
-  })
+    describe('Request to Revoke Consent ', () => {
+      it('Should initiate consent revoke request when all necessary fields are set', async () => {
+        const mojaloopClientSpy = jest
+          .spyOn(server.app.mojaloopClient, 'postRevokeConsent')
+          .mockImplementation()
 
-  it('Should initiate challenge generation request when all necessary fields are set', async () => {
-    const mojaloopClientSpy = jest
-      .spyOn(server.app.mojaloopClient, 'postGenerateChallengeForConsent')
-      .mockImplementation()
+        const validatorSpy = jest
+          .spyOn(Validator, 'isValidRevokeConsent')
+          .mockReturnValue(true)
 
-    const validatorSpy = jest
-      .spyOn(Validator, 'isValidChallengeGeneration')
-      .mockReturnValue(true)
+        // Mock the expected transaction request being sent.
+        const consentRevokeRequested = {
+          id: '111',
+          consentId: '2323',
+          party: {
+            partyIdInfo: {
+              partyIdType: PartyIdType.MSISDN,
+              partyIdentifier: '+1-222-222-2222',
+              fspId: 'fspb',
+            },
+          },
+          status: ConsentStatus.REVOKE_REQUESTED,
+        }
 
-    // Mock consent data that would be given by Firebase
-    const consentGenerateChallenge: Consent = {
-      id: '111',
-      consentId: '2323',
-      party: {
-        partyIdInfo: {
-          partyIdType: PartyIdType.MSISDN,
-          partyIdentifier: '+1-222-222-2222',
-          fspId: 'fspb',
-        },
-      },
-      status: ConsentStatus.ACTIVE,
-      scopes: [
-        {
-          accountId: 'as2342',
-          actions: ['account.getAccess', 'account.transferMoney'],
-        },
-        {
-          accountId: 'as22',
-          actions: ['account.getAccess'],
-        },
-      ],
-      consentRequestId: '12345',
-      authChannels: ['WEB'],
-      accounts: [
-        { id: 'bob.aaaaa.fspb', currency: Currency.SGD },
-        { id: 'bob.bbbbb.fspb', currency: Currency.USD },
-      ],
-    }
+        await consentsHandler.onUpdate(server, consentRevokeRequested)
 
-    await consentsHandler.onUpdate(server, consentGenerateChallenge)
-
-    expect(validatorSpy).toBeCalledWith(consentGenerateChallenge)
-    expect(mojaloopClientSpy).toBeCalledWith(
-      consentGenerateChallenge.consentId,
-      consentGenerateChallenge.party?.partyIdInfo.fspId
-    )
-  })
-
-  it('Should initiate PUT consent/{ID} request when challenge has been signed and all necessary fields are set', async () => {
-    const mojaloopClientSpy = jest
-      .spyOn(server.app.mojaloopClient, 'putConsentId')
-      .mockImplementation()
-
-    const validatorSpy = jest
-      .spyOn(Validator, 'isValidSignedChallenge')
-      .mockReturnValue(true)
-
-    // Mock the expected transaction request being sent.
-    const consentVerifiedChallenge = {
-      id: '111',
-      consentId: '2323',
-      initiatorId: 'pispa',
-      participantId: 'pispb',
-      scopes: [
-        {
-          accountId: 'as2342',
-          actions: ['account.getAccess', 'account.transferMoney'],
-        },
-        {
-          accountId: 'as22',
-          actions: ['account.getAccess'],
-        },
-      ],
-      party: {
-        partyIdInfo: {
-          partyIdType: PartyIdType.MSISDN,
-          partyIdentifier: '+1-222-222-2222',
-          fspId: 'fspb',
-        },
-      },
-      status: ConsentStatus.CHALLENGE_VERIFIED,
-      credential: {
-        id: '9876',
-        credentialType: 'FIDO' as const,
-        status: 'VERIFIED' as const,
-        challenge: {
-          payload: 'string_representing_challenge_payload',
-          signature: 'string_representing_challenge_signature',
-        },
-        payload: 'string_representing_credential_payload',
-      },
-    }
-
-    await consentsHandler.onUpdate(server, consentVerifiedChallenge)
-
-    expect(validatorSpy).toBeCalledWith(consentVerifiedChallenge)
-    expect(mojaloopClientSpy).toBeCalledWith(
-      consentVerifiedChallenge.consentId,
-      {
-        requestId: consentVerifiedChallenge.id,
-        initiatorId: consentVerifiedChallenge.initiatorId,
-        participantId: consentVerifiedChallenge.participantId,
-        scopes: consentVerifiedChallenge.scopes,
-        credential: consentVerifiedChallenge.credential,
-      },
-      consentVerifiedChallenge.party.partyIdInfo.fspId
-    )
-  })
-
-  it('Should initiate consent revoke request when all necessary fields are set', async () => {
-    const mojaloopClientSpy = jest
-      .spyOn(server.app.mojaloopClient, 'postRevokeConsent')
-      .mockImplementation()
-
-    const validatorSpy = jest
-      .spyOn(Validator, 'isValidRevokeConsent')
-      .mockReturnValue(true)
-
-    // Mock the expected transaction request being sent.
-    const consentRevokeRequested = {
-      id: '111',
-      consentId: '2323',
-      party: {
-        partyIdInfo: {
-          partyIdType: PartyIdType.MSISDN,
-          partyIdentifier: '+1-222-222-2222',
-          fspId: 'fspb',
-        },
-      },
-      status: ConsentStatus.REVOKE_REQUESTED,
-    }
-
-    await consentsHandler.onUpdate(server, consentRevokeRequested)
-
-    expect(validatorSpy).toBeCalledWith(consentRevokeRequested)
-    expect(mojaloopClientSpy).toBeCalledWith(
-      consentRevokeRequested.consentId,
-      consentRevokeRequested.party.partyIdInfo.fspId
-    )
+        expect(validatorSpy).toBeCalledWith(consentRevokeRequested)
+        expect(mojaloopClientSpy).toBeCalledWith(
+          consentRevokeRequested.consentId,
+          consentRevokeRequested.party.partyIdInfo.fspId
+        )
+      })
+    })
   })
 })
