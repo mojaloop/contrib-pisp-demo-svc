e4ae20bcce3e1eacb10ec6512ad567975064fb76
akapur99 <abhi.kapur09@gmail.com>
2020-09-08 13:25:19 +0530

    fix: linter warnings

--- src/shared/ml-thirdparty-simulator/factories/transfer.ts
+++ src/shared/ml-thirdparty-simulator/factories/transfer.ts
@@ -28,21 +28,24 @@ import * as faker from 'faker'
 import {
   AuthorizationsPutIdRequest,
   TransferIDPutRequest,
-} from '~/shared/ml-thirdparty-client/models/openapi';
+} from '~/shared/ml-thirdparty-client/models/openapi'
 
-import { TransferState } from '~/shared/ml-thirdparty-client/models/core';
+import { TransferState } from '~/shared/ml-thirdparty-client/models/core'
 
 export class TransferFactory {
   /**
    * Creates a `PUT /transfers/{ID}` request body that is normally sent
    * by Mojaloop as a callback to inform about the transfer result.
-   * 
+   *
    * @param _               transaction request id of the corresponsing authorization.
    * @param __              an authorization object as defined by the Mojaloop API.
    * @param transactionId   transaction id to be associated with the transfer object.
    */
-  public static createTransferIdPutRequest(_: string,
-    __: AuthorizationsPutIdRequest, transactionId: string): TransferIDPutRequest {
+  public static createTransferIdPutRequest(
+    _: string,
+    __: AuthorizationsPutIdRequest,
+    transactionId: string
+  ): TransferIDPutRequest {
     return {
       transactionId,
       fulfilment: faker.random.alphaNumeric(43),
