c1ebf8a17fd2aea6f2b1e52abd64e2326a929e80
akapur99 <abhi.kapur09@gmail.com>
2020-09-18 12:23:33 +0530

    feat: Implement new firebase mocking

--- src/lib/firebase/__mocks__/index.ts
+++ src/lib/firebase/__mocks__/index.ts
@@ -1,41 +1,41 @@
-/*****
- License
- --------------
- Copyright © 2020 Mojaloop Foundation
- The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the 'License') and you may not use these files except in compliance with the License. You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- Contributors
- --------------
- This is the official list of the Mojaloop project contributors for this file.
- Names of the original copyright holders (individuals or organizations)
- should be listed with a '*' in the first column. People who have
- contributed from an organization can be listed under the organization
- that actually holds the copyright for their contributions (see the
- Mojaloop Foundation organization for an example). Those individuals should have
- their names indented and be marked with a '-'. Email address can be added
- optionally within square brackets <email>.
- * Mojaloop Foundation
- - Name Surname <name.surname@mojaloop.io>
+import firebasemock from 'firebase-mock'
 
- * Google
- - Steven Wijaya <stevenwjy@google.com>
- --------------
- ******/
+// const mockauth = new firebasemock.MockAuthentication()
+// const mockdatabase = new firebasemock.MockFirebase()
+const mockfirestore = new firebasemock.MockFirestore()
+// const mockstorage = new firebasemock.MockStorage()
+// const mockmessaging = new firebasemock.MockMessaging()
+const mocksdk = new firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (_path: unknown) => {
+    // return path ? mockdatabase.child(path) : mockdatabase
+    return null
+  },
+  // use null if your code does not use AUTHENTICATION
+  () => {
+    // return mockauth
+    return null
+  },
+  // use null if your code does not use FIRESTORE
+  () => {
+    return mockfirestore
+  },
+  // use null if your code does not use STORAGE
+  () => {
+    // return mockstorage
+    return null
+  },
+  // use null if your code does not use MESSAGING
+  () => {
+    // return mockmessaging
+    return null
+  }
+)
 
-/**
- * A mock object to simulate the operations on Firebase.
- * Since there are a lot of chained methods from various classes,
- * in the current implementation, we only use a single mocked object 
- * to avoid unnecessary complexity.
- */
-export default {
-  firestore: jest.fn().mockReturnThis(),
-  collection: jest.fn().mockReturnThis(),
-  doc: jest.fn().mockReturnThis(),
-  where: jest.fn().mockReturnThis(),
-  add: jest.fn(),
-  set: jest.fn(),
-  update: jest.fn(),
-  onSnapshot: jest.fn().mockImplementation(() => jest.fn()),
-}
+export default mocksdk
+
+// console.log(mockauth)
+// console.log(mockdatabase)
+// console.log(mockmessaging)
+// console.log(mockstorage)
